a41135e7ce1d4192b1754f3af57ae85f0f2c6906
Bjorn Buckwalter <bjorn.buckwalter@gmail.com>
2008-10-16 21:10:15 -0400

    Removed old Time module.
    darcs-hash:20081017011015-03283-5bb99e0c84ac73caaa9b73d3760b4a283cbdb80a.gz

--- Astro/TestTime.hs
+++ Astro/TestTime.hs
@@ -10,31 +10,120 @@ import Test.QuickCheck
 import Data.Time hiding (utc)
 import Data.Time.Clock.TAI
 
+-- Preliminaries
+-- =============
+
+onceCheck = check (defaultConfig {configMaxTest = 1})
+
+-- | Comparison allowing for inaccuracy.
+cmpE :: (DiffEpoch t, Fractional a, Ord a) => Time a -> t -> t -> Bool
+cmpE accuracy t t' = abs (diffEpoch t t') < accuracy
+
 -- Accuracies.
 tdbError = 20 *~ micro second  -- About 10 us according to Kaplan, but for Vallado's cases the difference is greater...
 dblError =  1 *~ nano second
+noError  =  0 *~ second
 
-ttConverges  = tt  1977 01 01 00 00 32.184 == tai 1977 01 01 00 00 00.000
-tcgConverges = tcg 1977 01 01 00 00 32.184 == tai 1977 01 01 00 00 00.000
-tdbConverges = abs (tdb 1977 01 01 00 00 32.184 `diffEpoch` tai 1977 01 01 00 00 00.000) < tdbError -- Don't think this should pass after closer investigation!
+
+
+
+-- Specific epochs
+-- ===============
+
+-- J200 epoch.
+prop_J2000_jd    = j2000 ==  jd  2451545 TT
+prop_J2000_mjd   = j2000 == mjd  51544.5 TT
+prop_J2000_clock = j2000 == clock 2000 1 1 12 0 0 TT
+prop_J2000_TAI   = convert j2000 == clock 2000 1 1 11 59 27.816 TAI 
+-- prop_J2000_UTC   = convertUTC () j2000 == clock 2000 1 1 11 58 55.816 UTC
+
+-- Convergence epoch
+-- -----------------
+ttConverges  = clock 1977 01 01 00 00 32.184 TT  == convert (clock 1977 1 1 0 0 0 TAI)
+tcgConverges = clock 1977 01 01 00 00 32.184 TCG == convert (clock 1977 1 1 0 0 0 TAI)
+-- The accuracy of TDB and TCB are limited by the TDB conversions.
+tcbConverges = cmpE tdbError (clock 1977 01 01 00 00 32.184     TCB) (convert $ clock 1977 1 1 0 0 0 TAI) 
+tdbConverges = cmpE tdbError (clock 1977 01 01 00 00 32.1839345 TDB) (convert $ clock 1977 1 1 0 0 0 TAI) 
+-- This first test isn't adversely affected by conversion to Double while the second is.
+prop_TDBTCB_conv  = clock 1977 01 01 00 00 32.184 TCB == convert (clock 1977 01 01 00 00 32.1839345 TDB)
+prop_TDBTCB_conv' = cmpE dblError (convert $ clock 1977 01 01 00 00 32.184 TCB) (clock 1977 01 01 00 00 32.1839345 TDB)
+
+
+-- Conversion tests
+-- ================
+-- Check conversions back and forth are consistent. Test around both
+-- JD 0 and MJD 0 to make sure we aren't favoring algorithms that are
+-- only good around certain dates.
+prop_TAI, prop_TT, prop_TCG, prop_TDB, prop_TCB, prop_TCB' :: Double -> Bool
+-- These should be exact.
+prop_TAI a = let t =  jd a TAI in t == convert (convert t :: E TAI)  -- ^ Conversion back and forth to TAI.
+          && let t = mjd a TAI in t == convert (convert t :: E TAI)  -- ^ Conversion back and forth to TAI.
+prop_TT  a = let t =  jd a TT  in t == convert (convert t :: E TAI)  -- ^ Conversion back and forth to TAI.
+          && let t = mjd a TT  in t == convert (convert t :: E TAI)  -- ^ Conversion back and forth to TAI.
+-- In the remaining double precision errors come into play.
+prop_TCG a = let t =  jd a TCG in cmpE dblError t (convert (convert t :: E TAI))  -- ^ Conversion back and forth to TAI.
+          && let t = mjd a TCG in cmpE dblError t (convert (convert t :: E TAI))  -- ^ Conversion back and forth to TAI.
+-- Accuracy of TDB|TCB <-> TAI is limited by the accuracy of TDB <-> TAI. 
+-- However, the reverse conversion appears to cancels the error nicely.
+prop_TDB a = let t =  jd a TDB in cmpE dblError t (convert (convert t :: E TAI))  -- ^ Conversion back and forth to TAI.
+          && let t = mjd a TDB in cmpE dblError t (convert (convert t :: E TAI))  -- ^ Conversion back and forth to TAI.
+prop_TCB a = let t =  jd a TCB in cmpE dblError t (convert (convert t :: E TAI))  -- ^ Conversion back and forth to TAI.
+          && let t = mjd a TCB in cmpE dblError t (convert (convert t :: E TAI))  -- ^ Conversion back and forth to TAI.
+-- The accuracy of TCB <-> TDB conversions should be good. (This test is mostly redundant given prop_TCB).
+prop_TCB' a = let t =  jd a TCB in cmpE dblError t (convert (convert t :: E TDB))  -- ^ Conversion back and forth to TDB.
+           && let t = mjd a TCB in cmpE dblError t (convert (convert t :: E TDB))  -- ^ Conversion back and forth to TDB.
+
+
+
+-- Tests from literature
+-- =====================
 
 -- utc 1990 05 14 10 43 00.000 from [1].
-valladoExample = abs (tt 1990 05 14 16 43 57.184 `diffEpoch` tdb 1990 05 14 16 43 57.18527) < tdbError
+valladoExample = tai == convert tt
+              && cmpE tdbError (tai) (convert tdb)
+              && cmpE tdbError (tt ) (convert tdb)
+  where 
+    tai = clock 1990 05 14 16 43 25.000 TAI
+    tt  = clock 1990 05 14 16 43 57.184 TT
+    tdb = clock 1990 05 14 16 43 57.18527 TDB
+
 -- utc 2004 04 06 07 51 28.386009 from [2].
-valladoExample2 = tai' == tt'
-               && abs (diffEpoch tai' tcg') < dblError
-               && abs (diffEpoch tai' tdb') < tdbError
-               && abs (diffEpoch tdb' tcb') < dblError
-  where
-    tai' = tai 2004 04 06 7 52 00.386009
-    tt'  = tt  2004 04 06 7 52 32.570009
-    tcg' = tcg 2004 04 06 7 52 33.1695861742
-    tdb' = tdb 2004 04 06 7 52 32.5716651154
-    tcb' = tcb 2004 04 06 7 52 45.9109901113
+valladoExample2 = tai == convert tt && convert tai == tt
+               && cmpE dblError tai (convert tcg)    -- Test conversion from TCG.
+               && cmpE dblError (convert tai) tcg  -- Test conversion to TCG.
+               && cmpE tdbError tai (convert tdb)    -- Test conversion from TDB.
+               && cmpE tdbError (convert tai) tdb  -- Test conversion to TDB.
+               -- && cmpE dblError (tdbToTCB tdb) tcb         -- Test conversion to TCB.
+               -- && cmpE tdbError (fromTAI $ toTAI tai) tcb  -- Test conversion to TCB.
+  --where
+tai = clock 2004 04 06 7 52 00.386009 TAI
+tt  = clock 2004 04 06 7 52 32.570009 TT
+tcg = clock 2004 04 06 7 52 33.1695861742 TCG
+tdb = clock 2004 04 06 7 52 32.5716651154 TDB
+tcb = clock 2004 04 06 7 52 45.9109901113 TCB  -- Incorrect in example, no offset.
+
+
+-- Driver
+-- ======
 
 main = do
-  quickCheck ttConverges
-  quickCheck tcgConverges
-  quickCheck valladoExample
-  quickCheck valladoExample2
-  -- quickCheck tdbConverges -- TODO: fails!  
+  -- Start with the faster-running tests.
+  onceCheck prop_J2000_jd
+  onceCheck prop_J2000_mjd
+  onceCheck prop_J2000_clock
+  onceCheck prop_J2000_TAI
+  onceCheck prop_TDBTCB_conv
+  onceCheck prop_TDBTCB_conv'
+  onceCheck ttConverges
+  onceCheck tcgConverges
+  onceCheck tcbConverges
+  onceCheck tdbConverges
+  onceCheck valladoExample
+  onceCheck valladoExample2
+  quickCheck prop_TAI
+  quickCheck prop_TT
+  quickCheck prop_TCG
+  quickCheck prop_TDB
+  quickCheck prop_TCB
+  quickCheck prop_TCB'
+
