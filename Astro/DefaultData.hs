dff0663054ea43be68fe9944a89e04a15d1ef1a0
Bjorn Buckwalter <bjorn.buckwalter@gmail.com>
2009-01-17 09:37:24 -0500

    DefaultData uses LeapSecondTable from leapseconds-announced.

--- Astro/DefaultData.hs
+++ Astro/DefaultData.hs
@@ -5,10 +5,11 @@ import Astro.Time hiding (taiToUT1, ut1ToTAI)
 import qualified Astro.Time.Barycentric.Kaplan2005 as TDB
 import IAU2000.Nutation
 import qualified IAU2000.Equations as Eq
+import Data.Time.Clock.AnnouncedLeapSeconds
 
 
 defaultTimeData = TimeData
-  { leapSecondTable = const 33
+  { leapSecondTable = lst
   , taiToUT1 = \(E t) -> E t  -- Astro.Time.taiToUT1 (const 0) ??
   , ut1ToTAI = \(E t) -> E t
   ,  ttToTDB = TDB.ttToTDB
