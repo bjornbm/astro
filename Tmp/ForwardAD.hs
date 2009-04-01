eacb1c18951fd38219f5fb8a20ff9566a1fdb92d
Bjorn Buckwalter <bjorn.buckwalter@gmail.com>
2009-03-31 21:21:41 -0400

    Changes to be compatible with fad-1.0.

--- Tmp/ForwardAD.hs
+++ Tmp/ForwardAD.hs
@@ -5,19 +5,19 @@
 module Tmp.ForwardAD (diff, Lift, lift) where
 
 import Numeric.Units.Dimensional (Dimensional (Dimensional), Quantity, Div)
-import Fad (Dual)
-import qualified Fad (diff, lift)
+import Numeric.FAD (Dual)
+import qualified Numeric.FAD (diff, lift)
 
 diff :: (Num a, Div d2 d1 d2')
      => (forall tag. Quantity d1 (Dual tag a) -> Quantity d2 (Dual tag a))
      -> Quantity d1 a -> Quantity d2' a
-diff f (Dimensional x) = Dimensional (Fad.diff f' x)
+diff f (Dimensional x) = Dimensional (Numeric.FAD.diff f' x)
   where
     f' = undim . f . Dimensional
     undim (Dimensional a) = a
 
 
 class Lift w where lift :: Num a => w a -> w (Dual tag a)
-instance Lift (Dimensional v d) where lift (Dimensional x) = Dimensional (Fad.lift x)
+instance Lift (Dimensional v d) where lift (Dimensional x) = Dimensional (Numeric.FAD.lift x)
 
 
