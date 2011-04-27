651899f4a3057ee02db5ae81368e5c01b5dea455
Bjorn Buckwalter <bjorn@buckwalter.se>
2011-04-27 22:05:48 +0800

    Clean up MEOE.

--- MEOE.hs
+++ MEOE.hs
@@ -13,8 +13,7 @@ import COE
 
 type SV a = (CPos a, CVel a)
 
--- | Modified Equinoctial Orbital Elements as defined by Walker
--- et al.
+-- | Modified Equinoctial Orbital Elements as defined by Walker et al.
 data MEOE a = MEOE
   { mu :: GravitationalParameter a
   , p  :: Length a
@@ -62,49 +61,20 @@ meoe2coe MEOE{..} = COE
 -- | Convert a MEOE to a cartesian State Vector.
 meoe2sv :: RealFloat a => MEOE a -> SV a
 meoe2sv MEOE{..} = ( (r_x <: r_y <:. r_z) >* (r / s2)
-                   , (v_x <: v_y <:. v_z) >* (sqrt (mu / p) / s2))
+                   , (v_x <: v_y <:. v_z) >* negate (sqrt (mu / p) / s2)
+                   )
   where
     -- Position
-    r_x = cos l + alpha2 * cos l + _2 * h * k * sin l
-    r_y = sin l - alpha2 * sin l + _2 * h * k * cos l
+    r_x = cos l + a2 * cos l + _2 * h * k * sin l
+    r_y = sin l - a2 * sin l + _2 * h * k * cos l
     r_z = _2 * (h * sin l - k * cos l)
     -- Velocity
-    v_x = negate _1 * (        sin l  + alpha2 * sin l - _2 * h * k * cos l + g - _2 * f * h * k + alpha2 * g)
-    v_y = negate _1 * (negate (cos l) + alpha2 * cos l + _2 * h * k * sin l - f + _2 * g * h * k + alpha2 * f)
-    v_z = _2 * (h * cos l + k * sin l + f * h + g * k)
+    v_x =         sin l  + a2 * sin l - hk2 * cos l + g - f * hk2 + a2 * g
+    v_y = negate (cos l) + a2 * cos l + hk2 * sin l - f + g * hk2 + a2 * f
+    v_z = negate _2 * (h * cos l + k * sin l + f * h + g * k)
     -- where
-    alpha2 = h ^ pos2 - k ^ pos2
+    a2 = h ^ pos2 - k ^ pos2  -- alpha^2
     s2 = _1 + h ^ pos2 + k ^ pos2
     w  = _1 + f * cos l + g * sin l
     r  = p / w
-
-
-{-
-sv2meoe :: RealFloat a => GravitationalParameter a -> CPos a -> CVel a -> MEOE a
-sv2meoe mu r' v' = MEOE
-  { mu = mu
-  , p  = h ^ pos2 / mu
-  , f  = undefined -- ecc * cos (aop + raan)
-  , g  = undefined -- ecc * sin (aop + raan)
-  , h  = undefined -- tan (inc / _2) * cos raan
-  , k  = undefined -- tan (inc / _2) * sin raan
-  , l  = undefined -- raan + aop + trueAnomaly
-  }
-  where
-    r  = vNorm r'
-    (x,y,z) = toTuple r'
-    v  = vNorm v'
-    h' = r' `crossProduct` v'
-    h  = vNorm h'
-    (h_x, h_y, h_z) = toTuple h'
-    raan = atan2 (h_x) (negate h_y)
-    --trueAnomaly = atan2 (h * r * v) (h ^ pos2 - mu * r)
-    u = acos ((x * cos raan + y * sin raan) / r)
-    e' = v' `crossProduct` h' >/ mu >-< vNormalize r'
-    e  = vNorm e'
-    trueAnomaly = sideOfPeri * acos (r' >.< e' / (r * e))
-    sideOfPeri = negate $ r' >.< v' / (r * v)
--}
-
-r::CPos Double;r=undefined
-v::CVel Double;v=undefined
+    hk2 = _2 * h * k
