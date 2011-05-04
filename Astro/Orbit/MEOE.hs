bc46d66fe49e3f960a30140e8a7e8e74ec811db0
Bjorn Buckwalter <bjorn@buckwalter.se>
2011-05-04 14:37:23 +0200

    Convert vector to MEOE.

--- Astro/Orbit/MEOE.hs
+++ Astro/Orbit/MEOE.hs
@@ -3,6 +3,7 @@
 
 module Astro.Orbit.MEOE where
 
+import Numeric.NumType (incr)
 import Numeric.Units.Dimensional.Prelude
 import Numeric.Units.Dimensional.LinearAlgebra
 import Astro.Orbit.Types
@@ -22,3 +23,11 @@ data MEOE long a = MEOE
 -- | Convert a MEOE into a vector (not a State Vector) of its
 -- elements (including mu).
 meoe2vec MEOE{..} = mu <: p <: f <: g <: h <: k <:. long longitude
+
+vec2meoe v = MEOE (vElemAt zero v)
+                  (vElemAt pos1 v)
+                  (vElemAt pos2 v)
+                  (vElemAt pos3 v)
+                  (vElemAt pos4 v)
+                  (vElemAt pos5 v)
+                  (Long $ vElemAt (incr pos5) v)
