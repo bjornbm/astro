92ae1396acd3dc41a687d3af85b0b3c49cfa643c
Bjorn Buckwalter <bjorn@buckwalter.se>
2011-05-31 15:43:24 +0800

    Monadic main for tests.

--- test/Test.hs
+++ test/Test.hs
@@ -4,13 +4,7 @@ import qualified Astro.Orbit.Conversion.Tests
 import qualified Astro.Orbit.Interpolate.Tests
 import Test.Hspec.Monadic
 
-main = do
-  Astro.Orbit.Types.Tests.main
-  Astro.Orbit.Anomaly.Tests.main
-  Astro.Orbit.Conversion.Tests.main
-  Astro.Orbit.Interpolate.Tests.main
-
-main2 = hspec $ do
+main = hspec $ do
   Astro.Orbit.Types.Tests.specs
   Astro.Orbit.Anomaly.Tests.specs
   Astro.Orbit.Conversion.Tests.specs
