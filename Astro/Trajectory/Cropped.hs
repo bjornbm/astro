cace161c5adf167683170e19c2ffb45a837b2410
Björn Buckwalter <bjorn@buckwalter.se>
2013-08-02 12:55:52 +0200

    Type rename.

--- Astro/Trajectory/Cropped.hs
+++ Astro/Trajectory/Cropped.hs
@@ -11,12 +11,12 @@ import Astro.Trajectory
 
 -- Wrapper data type used to crop the validity (in time) of an
 -- arbitrary @Trajectory@.
-data CroppedTraj x t a = CS (E t a) (x t a)
+data Cropped x t a = CS (E t a) (x t a)
                        | CE (E t a) (x t a)
                        | C0 (x t a)
-                       | C2 (CroppedTraj (CroppedTraj x) t a)
+                       | C2 (Cropped (Cropped x) t a)
 
-instance (Trajectory x t a) => Trajectory (CroppedTraj x) t a where
+instance (Trajectory x t a) => Trajectory (Cropped x) t a where
   startTime (CS t0 traj) = max t0 (startTime traj)
   startTime (CE _  traj) = startTime traj
   startTime (C0    traj) = startTime traj
@@ -49,24 +49,24 @@ instance (Trajectory x t a) => Trajectory (CroppedTraj x) t a where
 
 -- | @cropStart t0 traj@ crops @traj@ so it starts no earlier than @t0@.
 -- If @startTime traj >= t0@ the @Trajectory@ is effectively unchanged.
-cropStart :: Trajectory x t a => E t a -> x t a -> CroppedTraj x t a
+cropStart :: Trajectory x t a => E t a -> x t a -> Cropped x t a
 cropStart t0 x = CS t0 x
 
 -- | @cropEnd t1 traj@ crops @traj@ so it end no later than @t1@.
 -- If @endTime traj <= t1@ the @Trajectory@ is effectively unchanged.
-cropEnd :: Trajectory x t a => E t a -> x t a -> CroppedTraj x t a
+cropEnd :: Trajectory x t a => E t a -> x t a -> Cropped x t a
 cropEnd t1 x = CE t1 x
 
-crop :: Trajectory x t a => E t a -> E t a -> x t a -> CroppedTraj x t a
+crop :: Trajectory x t a => E t a -> E t a -> x t a -> Cropped x t a
 crop t0 t1 = C2 . cropStart t0 . cropEnd t1
 
-cropStart' :: Trajectory x t a => Maybe (E t a) -> x t a -> CroppedTraj x t a
+cropStart' :: Trajectory x t a => Maybe (E t a) -> x t a -> Cropped x t a
 cropStart' Nothing = C0
 cropStart' (Just t0) = cropStart t0
-cropEnd'   :: Trajectory x t a => Maybe (E t a) -> x t a -> CroppedTraj x t a
+cropEnd'   :: Trajectory x t a => Maybe (E t a) -> x t a -> Cropped x t a
 cropEnd'   Nothing = C0
 cropEnd'   (Just t1) = cropEnd t1
-crop' :: Trajectory x t a => Maybe (E t a) -> Maybe (E t a) -> x t a -> CroppedTraj x t a
+crop' :: Trajectory x t a => Maybe (E t a) -> Maybe (E t a) -> x t a -> Cropped x t a
 crop' Nothing   Nothing   = C0
 crop' (Just t0) Nothing   = cropStart t0
 crop' Nothing   (Just t1) = cropEnd t1
