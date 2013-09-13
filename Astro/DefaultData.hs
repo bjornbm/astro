eb588bd70970e1a6348158ccbb813cd763136fcb
Bjorn Buckwalter <bjorn@buckwalter.se>
2013-09-13 14:15:55 +0200

    Data.Default instances for AstroData.

--- Astro/DefaultData.hs
+++ Astro/DefaultData.hs
@@ -7,8 +7,9 @@ import qualified Astro.Time.Barycentric.Kaplan2005 as TDB
 import IAU2000.Nutation
 import qualified IAU2000.Equations as Eq
 import Data.Time.Clock.AnnouncedLeapSeconds
+import Data.Default
 
-
+defaultTimeData :: Floating a => TimeData a
 defaultTimeData = TimeData
   { leapSecondTable = lst
   , taiToUT1 = coerce  -- Astro.Time.taiToUT1 (const 0) ??
@@ -17,14 +18,21 @@ defaultTimeData = TimeData
   , tdbToTT  = TDB.tdbToTT
   }
 
+defaultNutationModel :: Floating a => NutationModel a
 defaultNutationModel = NutationModel
   { angles              = nutationAngles2000A
   , equationOfEquinoxes = Eq.equationOfEquinoxes
   }
 
+
+defaultAstroData :: Floating a => AstroData a
 defaultAstroData = AstroData
   { time         = defaultTimeData
   , nutation     = defaultNutationModel
   , refEllipsoid = iers2003
   }
 
+-- TODO: How do I best avoid these being orphan instances?
+instance Floating a => Default (TimeData a) where def = defaultTimeData
+instance Floating a => Default (NutationModel a) where def = defaultNutationModel
+instance Floating a => Default (AstroData a) where def = defaultAstroData
--- Astro/Place/ReferenceEllipsoid.hs
+++ Astro/Place/ReferenceEllipsoid.hs
@@ -26,8 +26,8 @@ referenceEllipsoid' :: Fractional a => EquatorialRadius a -> InverseFlattening a
 --  g = 1 / f  and  f = 1 - b / a  ==>  b = a * (1 - 1 / g) = a - a / g
 referenceEllipsoid' a g = ReferenceEllipsoid a b where b = a - a / g
 
-wgs84, grs80 :: Fractional a => ReferenceEllipsoid a
-
+clarke1866, bessel1841, hayford1910, international1924, krasovsky1940, grs80,
+  wgs84, iers2003 :: Fractional a => ReferenceEllipsoid a
 clarke1866        = ReferenceEllipsoid  (6378206.4  *~meter) (6356583.8*~meter)  -- [wp].
 bessel1841        = referenceEllipsoid' (6377397.155*~meter) (299.1528153513233*~one)
 hayford1910       = referenceEllipsoid' (6378388.0  *~meter) (297*~one)          -- [wp].
