ffff83a96f009b308e84e2f706edbc0e5fbbdade
Bjorn Buckwalter <bjorn@buckwalter.se>
2014-10-20 12:27:20 +0200

    Tests for perfectGEO.

--- test/Astro/UtilSpec.hs
+++ test/Astro/UtilSpec.hs
@@ -14,11 +14,38 @@ import Numeric.Units.Dimensional.Prelude
 import qualified Prelude
 
 import Astro.Util
-
+import Astro.Util.Cyclic
+import Astro.Coords
+import Astro.Coords.PosVel
+import Numeric.Units.Dimensional.LinearAlgebra
+import qualified Prelude
 
 
 main = hspec spec
 spec = do
-  return ()
+  spec_perfectGEO
+  spec_perfectGEO'
 
--- All specs have been moved to Astro.Util.CyclicSpec!
+
+spec_perfectGEO = describe "perfectGEO" $ do
+
+  it "has the desired radius."
+    (property $ \(l::Angle Double) -> (vElemAt zero $ s $ perfectGEO l) ~== r_GEO)
+
+  it "has the desired longitude."
+    (property $ \(l::Angle Double) -> (vElemAt pos2 $ s $ perfectGEO l) ~== plusMinusPi l)
+
+  it "has the desired zenith angle in ECR."
+    (property $ \(l::Angle Double) -> (vElemAt pos1 $ s $ perfectGEO l) == tau / _4)
+
+  it "does not have an out-of-plane component (cartesian)."
+    (property $ \(l::Angle Double) -> (vElemAt pos2 $ c $ perfectGEO l) == _0)
+
+
+spec_perfectGEO' = describe "perfectGEO'" $ do
+
+  it "has zero velocity in ECR frame."
+    (property $ \(l::Angle Double) -> (vNorm $ cvel $ perfectGEO' l) == _0)
+
+  it "has zero velocity in ECR frame (spherical)."
+    (property $ \(l::Angle Double) -> (vElemAt zero $ svel $ perfectGEO' l) == _0)
