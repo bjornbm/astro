ff97b25f77d327390f271fddc4b7ca3d9825a865
Bjorn Buckwalter <bjorn@buckwalter.se>
2014-10-20 12:50:21 +0200

    More ECR specs.

--- test/Astro/Coords/ECRSpec.hs
+++ test/Astro/Coords/ECRSpec.hs
@@ -24,10 +24,33 @@ import qualified Prelude
 main = hspec spec
 spec = do
   spec_ecrToECI
+  spec_eciToECR
 
 
 -- ----------------------------------------------------------
 spec_ecrToECI = describe "ecrToECI" $ do
+
   it "doesn't add out-of plane components to a perfect GEO"
-    (property $ \(l::Angle Double) t ->
+    (property $ \(l::Angle D) t ->
       (vElemAt pos2 $ c $ ecrToECI t $ perfectGEO l) == _0)
+
+  it "is the inverse of eciToECR"
+    (property $ \(p::Coord ECR D) t ->
+      (eciToECR t . ecrToECI t) p ~== p)
+
+  it "does not change the out-of-plane component"
+    (property $ \(p::Coord ECR D) t ->
+      vElemAt pos2 (c $ ecrToECI t p) == vElemAt pos2 (c p))
+
+  it "does not change the radius"
+    (property $ \(p::Coord ECR D) t ->
+      vElemAt zero (s $ecrToECI t p) ~== vElemAt zero (s p))
+
+
+-- ----------------------------------------------------------
+spec_eciToECR = describe "eciToECR" $ do
+
+  it "is the inverse of ecrToECI"
+    (property $ \(p::Coord ECI D) t ->
+      (ecrToECI t . eciToECR t) p ~== p)
+
