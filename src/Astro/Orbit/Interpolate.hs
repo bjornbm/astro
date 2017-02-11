d9accaa14f810304a7df51e1544402faf02c609c
Björn Buckwalter <bjorn@buckwalter.se>
2017-02-11 19:33:32 +0100

    linearPolateMEOEm without using meoe2vecUnsafe.

--- src/Astro/Orbit/Interpolate.hs
+++ src/Astro/Orbit/Interpolate.hs
@@ -86,16 +86,23 @@ linearPolateVecT (v0`At`t0) (v1`At`t1) t = linearPolateVec0 v0 (d t1,v1) (d t)
 -- NOTE: this does not work for hyperbolic orbits, and probably not
 -- when t1 and t0 are separated by more than one (or a half?) orbital
 -- period?
-linearPolateMEOEm :: RealFloat a => Datum t a -> Datum t a
-                  -> E t a -> MEOE Mean a
-linearPolateMEOEm m0 m1 t = vec2meoeUnsafe $
-  linearPolateVecT (meoe2vecUnsafe <$> m0) (meoe2vecUnsafe <$> m1') t
+linearPolateMEOEm :: RealFloat a
+                  => Datum t a -> Datum t a -> E t a -> MEOE Mean a
+linearPolateMEOEm m0 m1 t = MEOE
+  (linearPolateT (mu <$> m0) (mu <$> m1') t)
+  (linearPolateT (p  <$> m0) (p  <$> m1') t)
+  (linearPolateT (f  <$> m0) (f  <$> m1') t)
+  (linearPolateT (g  <$> m0) (g  <$> m1') t)
+  (linearPolateT (h  <$> m0) (h  <$> m1') t)
+  (linearPolateT (k  <$> m0) (k  <$> m1') t)
+  (Long $ linearPolateT (long . longitude <$> m0) (long . longitude <$> m1') t)
   where
     m1' = (\m -> m { longitude = Long l1' }) <$> m1
     l0 = long . longitude <$> m0
     l1 = long . longitude <$> m1
     l1' = adjustCyclicT l0 l1 period tau
     period = (meoeOrbitalPeriod (value m0) + meoeOrbitalPeriod (value m1)) / _2
+
 
 -- {-
 linearPolateMEOEm_OLD :: RealFloat a
